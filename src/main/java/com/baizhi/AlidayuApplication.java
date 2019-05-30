171b8b5ed37233ab7b1b6d4d46228ec5a26bf23c
huzx <huzxdyx@126.com>
2019-05-30 11:36:35 +0800

    v1.0

---
+++ src/main/java/com/baizhi/AlidayuApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlidayuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlidayuApplication.class, args);
+    }
+
+}
